b551eb4fc70f493fe324a406fed2295dd8b0ffd3
ghussaink <ghulamhussain.developer@gmail.com>
2025-07-23 18:34:25 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ghussaink.github.io/",
+  base: process.env.NODE_ENV === "development"
+    ? "/"
+    : process.env.VITE_BASE_PATH || "/",
+
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
@@ -22,5 +25,6 @@ export default defineConfig({
   server: {
     // @ts-ignore
     allowedHosts: true,
-  }
+  },
 });
+
